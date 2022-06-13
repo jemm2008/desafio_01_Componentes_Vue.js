47d4ee967a87fa52e13586ac477a566983d22bcf
jemm2008 <jemm2008@gmail.com>
2022-06-13 11:18:42 -0400

    Se sube soluciOn al desafio 1 Componentes - Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <!--<img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <h1 class="bg-info rounded-pill mx-5"> Nuevo Componente: <span class="fw-bold">{{title}}</span></h1>
+        <MyAxies v-for="axie in axies" :key="axie.id"
+            :axieName="axie.nombre"
+            :axieImg="axie.src"
+            :axieid= "axie.id"
+            :axiepage= "axie.page"
+        />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
+    import MyAxies from '@/components/MyAxies.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'App',
+        data(){
+            return {
+                title: "My Axies",
+                axies: [
+                    {
+                        id: 3179575,
+                        nombre: "Faster Fish!!",
+                        src: "https://axiecdn.axieinfinity.com/axies/3179575/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/3179575/"
+                    },
+                    {
+                        id: 2479271,
+                        nombre: "Pollito PIO_!!",
+                        src: "https://axiecdn.axieinfinity.com/axies/2479271/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/2479271/"
+                    },
+                    {
+                        id: 2278510,
+                        nombre: "Jack Sparrow_!!",
+                        src: "https://axiecdn.axieinfinity.com/axies/2278510/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/2278510/"
+                    },
+                    {
+                        id: 1962478,
+                        nombre: "Hércules VG01",
+                        src: "https://axiecdn.axieinfinity.com/axies/1962478/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/1962478/"
+                    },
+                    {
+                        id: 8641768,
+                        nombre: "virgin scaly",
+                        src: "https://axiecdn.axieinfinity.com/axies/8641768/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/8641768/"
+                    },
+                    {
+                        id: 1409689,
+                        nombre: "DapiDOX_!! Jr. VG3",
+                        src: "https://axiecdn.axieinfinity.com/axies/1409689/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/1409689/"
+                    },     
+                    {
+                        id: 10874427,
+                        nombre: "BestBird",
+                        src: "https://axiecdn.axieinfinity.com/axies/10874427/axie/axie-full-transparent.png",
+                        page: "https://marketplace.axieinfinity.com/axie/10874427/"
+                    }
+                ]
+            }
+        },
+        components: {
+            // HelloWorld,
+            MyAxies
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    body {
+        background-image: url('@/assets/imagenfondo.png'); 
+    }
+    span {
+        color: red;
+    }
 </style>
---
+++ src/components/MyAxies.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-9">
+            <div class="container d-flex align-items-center justify-content-around border border-4 rounded-pill mt-3">
+                <img class="bg-dark rounded-circle my-1" :src="axieImg" alt="axie_pic" width="125" height="120">
+                <div class="">
+                    <div class="d-flex">
+                        <h4 class="fw-bold mt-2 ms-5">{{axieName}}</h4>
+                        <h4 class="text-primary fw-bold mt-2 ms-5">{{axieid}}</h4>
+                    </div>
+                    <a class=" fs-4 fw-bold text-success" :href="axiepage" target="#blank">MySelf FAN page !!!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyAxies',
+    props: {
+        axieName: {
+            type: String,
+            required: true,
+        },
+        axieImg: {
+            type: String,
+            required: true,
+        },
+        axieid: {
+            type: Number,
+            required: true,
+        },
+        axiepage: {
+            type: String,
+            required: true,
+        },
+    },
+    //   data: function(){
+    //       return { }
+    //   },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+
+</style>
